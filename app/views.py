2adac2c7811c5e9b6136ae03881f89610a252d1f
donguty <carlos.joaquin.gutierrez@gmail.com>
2020-05-11 18:00:05 +0200

    errot_handler

--- app/views.py
+++ app/views.py
@@ -4,7 +4,12 @@ from flask import Blueprint,render_template
 
 page = Blueprint('page',__name__)
 
+@page.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
+
 @page.route('/')
 @page.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title = 'Index')
